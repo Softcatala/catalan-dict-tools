b436913d2f66d889bfb73a78b5c40afa6d6e27b2
Jaume Ortolà <jaumeortola@gmail.com>
2015-01-01 21:15:24 +0100

    arregla adreça a fitxer

--- fdic-to-hunspell/build.sh
+++ fdic-to-hunspell/build.sh
@@ -6,4 +6,4 @@ perl fdic-to-hunspell-noms-adj.pl
 perl fdic-to-hunspell-resta.pl
 cat *.dic > resultat/dictionaries/catalan.dic
 export LC_ALL=C && sort -u resultat/dictionaries/catalan.dic -o resultat/dictionaries/catalan.dic
-cat header.aff afixos-no-verbs.aff modelsverbals.aff > resultat/dictionaries.catalan.aff
+cat header.aff afixos-no-verbs.aff modelsverbals.aff > resultat/dictionaries/catalan.aff
