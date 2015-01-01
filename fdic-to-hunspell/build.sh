5d931fc884fc01b3e4178d062267c358aba3284b
Jaume Ortolà <jaumeortola@gmail.com>
2015-01-01 20:16:18 +0100

    nou build hunspell

--- fdic-to-hunspell/build.sh
+++ fdic-to-hunspell/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#perl fdic-to-hunspell-verbs.pl
-#perl fdic-to-hunspell-noms-adj.pl
-#perl fdic-to-hunspell-resta.pl
+perl fdic-to-hunspell-verbs.pl
+perl fdic-to-hunspell-noms-adj.pl
+perl fdic-to-hunspell-resta.pl
 cat *.dic > resultat/dictionaries/catalan.dic
 export LC_ALL=C && sort -u resultat/dictionaries/catalan.dic -o resultat/dictionaries/catalan.dic
 cat header.aff afixos-no-verbs.aff modelsverbals.aff > resultat/dictionaries.catalan.aff
