83de637ea900e9560f938af1beb069b57ece314c
Jaume Ortolà <jaumeortola@gmail.com>
2015-01-02 01:08:49 +0100

    crea .oxt

--- fdic-to-hunspell/build.sh
+++ fdic-to-hunspell/build.sh
@@ -7,3 +7,6 @@ perl fdic-to-hunspell-resta.pl
 cat *.dic > resultat/dictionaries/catalan.dic
 export LC_ALL=C && sort -u resultat/dictionaries/catalan.dic -o resultat/dictionaries/catalan.dic
 cat header.aff afixos-no-verbs.aff modelsverbals.aff > resultat/dictionaries/catalan.aff
+cd resultat
+rm ~/Baixades/catalan.oxt
+zip -r ~/Baixades/catalan.oxt *
