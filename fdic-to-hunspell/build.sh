735b541b7712b76e9ce909c765d0424aea502bb7
jaumeortola <jaumeortola@gmail.com>
2015-01-03 23:29:39 +0100

    afegeix nombre de línies a arxiu catalan.dic

--- fdic-to-hunspell/build.sh
+++ fdic-to-hunspell/build.sh
@@ -6,6 +6,13 @@ perl fdic-to-hunspell-noms-adj.pl
 perl fdic-to-hunspell-resta.pl
 cat *.dic > resultat/dictionaries/catalan.dic
 export LC_ALL=C && sort -u resultat/dictionaries/catalan.dic -o resultat/dictionaries/catalan.dic
+cd resultat/dictionaries
+cat catalan.dic | wc -l > linies.txt
+cat linies.txt catalan.dic > tmp.dic
+rm linies.txt
+rm catalan.dic
+mv tmp.dic catalan.dic
+cd ../..
 cat header.aff afixos-no-verbs.aff model_cantar_sense_apostrofacio.aff modelsverbals.aff > resultat/dictionaries/catalan.aff
 cd resultat
 rm ~/Baixades/catalan.oxt
