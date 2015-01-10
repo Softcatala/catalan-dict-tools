ae1843925a7de6385a242f77e5b232440234f92a
jaumeortola <jaumeortola@gmail.com>
2015-01-10 11:50:55 +0100

    crea dos diccionaris hunspell, de moment idèntics

--- build-new-hunspell.sh
+++ build-new-hunspell.sh
@@ -5,30 +5,37 @@ dir_intermedi="$dir_programes/intermedi"
 dir_arrel="diccionari-arrel"
 dir_resultat="resultats/hunspell"
 
-mkdir $dir_intermedi
-echo "Generant fitxers d'afixos"
-perl $dir_programes/genera-afixos-hunspell.pl $dir_dades/regles.hunspell $dir_intermedi/afixos-no-verbs.aff
-perl $dir_programes/genera-modelsverbals-hunspell.pl $dir_arrel/models-verbals $dir_intermedi/modelsverbals.aff
-echo "Generant diccionari de verbs"
-perl $dir_programes/fdic-to-hunspell-verbs.pl $dir_arrel/verbs-fdic.txt $dir_intermedi/verbs.dic $dir_arrel/models-verbals
-echo "Generant diccionari de noms i adjectius"
-perl $dir_programes/fdic-to-hunspell-noms-adj.pl $dir_arrel $dir_intermedi $dir_dades/regles.hunspell
-echo "Generant diccionari de la resta de categories"
-perl $dir_programes/fdic-to-hunspell-resta.pl $dir_arrel $dir_intermedi/resta.dic
 
-cp $dir_dades/*.dic $dir_intermedi
-cp $dir_dades/*.aff $dir_intermedi
+for variant in catalan catalan-valencia
+do
+    echo "*** Generant diccionari: $variant"
+    mkdir $dir_intermedi
+    echo "Generant fitxers d'afixos"
+    perl $dir_programes/genera-afixos-hunspell.pl $dir_dades/regles.hunspell $dir_intermedi/afixos-no-verbs.aff
+    perl $dir_programes/genera-modelsverbals-hunspell.pl $dir_arrel/models-verbals $dir_intermedi/modelsverbals.aff
+    echo "Generant diccionari de verbs"
+    perl $dir_programes/fdic-to-hunspell-verbs.pl $dir_arrel/verbs-fdic.txt $dir_intermedi/verbs.dic $dir_arrel/models-verbals
+    echo "Generant diccionari de noms i adjectius"
+    perl $dir_programes/fdic-to-hunspell-noms-adj.pl $dir_arrel $dir_intermedi $dir_dades/regles.hunspell
+    echo "Generant diccionari de la resta de categories"
+    perl $dir_programes/fdic-to-hunspell-resta.pl $dir_arrel $dir_intermedi/resta.dic
 
-cd $dir_intermedi
-cat *.dic > catalan.dic
-export LC_ALL=C && sort -u catalan.dic -o catalan.dic
-cat catalan.dic | wc -l > linies.txt
-cat linies.txt catalan.dic > tmp.dic
-rm catalan.dic
-mv tmp.dic catalan.dic
-cat header.aff afixos-no-verbs.aff model_cantar_sense_apostrofacio.aff modelsverbals.aff > catalan.aff
-cd -
-rm $dir_resultat/catalan.*
-cp $dir_intermedi/catalan.* $dir_resultat
-rm -rf $dir_intermedi
+    cp $dir_dades/*.dic $dir_intermedi
+    cp $dir_dades/*.aff $dir_intermedi
+
+    cd $dir_intermedi
+    cat *.dic > $variant.dic
+    export LC_ALL=C && sort -u $variant.dic -o $variant.dic
+    cat $variant.dic | wc -l > linies.txt
+    cat linies.txt $variant.dic > tmp.dic
+    rm $variant.dic
+    mv tmp.dic $variant.dic
+    cat header.aff afixos-no-verbs.aff model_cantar_sense_apostrofacio.aff modelsverbals.aff > $variant.aff
+    cd -
+    rm $dir_resultat/$variant.*
+    cp $dir_intermedi/$variant.* $dir_resultat
+    rm -rf $dir_intermedi
+done
+
+
 echo "FET. Resultats en $dir_resultat"
