5b2b59ec044e567547cb0205b27d3a737e762b79
Toni Hermoso Pulido <toniher@cau.cat>
2015-02-14 12:06:14 +0100

    scripts xpi

--- README.md
+++ README.md
@@ -24,6 +24,8 @@ Construeix el diccionari amb etiquetatge gramatical per al corrector Languagetoo
 
 ##### buid-xpi-all.sh
 Genera fitxers xpi (compatibles amb el Firefox) a partir dels resultats per al Hunspell.
+* ./build-xpi-all.sh -> Genera paquets de desenvolupament (versió 9.9.9)
+* ./build-xpi-all.sh -p -v '3.0.0' -> Genera paquets de producció (versió 3.0.0)
 
 ### Per fer
 * En el diccionari arrel marcar les fonts d'origen de cada paraula. Això servirà per a comprovar la correcció de les dades.
--- build-xpi-all.sh
+++ build-xpi-all.sh
@@ -1,16 +1,62 @@
+ID="dev.dictionaries.addons.mozilla.org"
+TAG="-test"
+VERSION="9.9.9"
+DEV=".dev"
+
+while getopts "pv:" opt; do
+    case "$opt" in
+    p)  ID="dictionaries.addons.mozilla.org"; TAG=""; DEV="";
+        ;;
+    v)  VERSION=$OPTARG;
+        ;;
+    esac
+done
+
+echo $ID
+echo $TAG
+echo $VERSION
+
 cd xpi/ca
+
+cp config_build.sh.pre config_build.sh
+cp install.rdf.pre install.rdf
+cp install.js.pre install.js
+
+sed -i -e s/\#\#VERSION\#\#/$VERSION/g config_build.sh
+sed -i -e s/\#\#DEV\#\#/$DEV/g config_build.sh
+
+sed -i -e s/\#\#ID\#\#/$ID/g install.rdf install.js
+sed -i -e s/\#\#TAG\#\#/$TAG/g install.rdf install.js
+sed -i -e s/\#\#VERSION\#\#/$VERSION/g install.rdf install.js
+
 rm -rf dictionaries
 mkdir dictionaries
 cp -rf ../../resultats/hunspell/catalan.aff dictionaries/ca.aff
 cp -rf ../../resultats/hunspell/catalan.dic dictionaries/ca.dic
 ../../build-xpi.sh config_build.sh
 rm -rf dictionaries
+rm -rf install.rdf install.js config_build.sh
 
+# Valencia
 cd ../ca-valencia
+
+cp config_build.sh.pre config_build.sh
+cp install.rdf.pre install.rdf
+cp install.js.pre install.js
+
+sed -i -e s/\#\#VERSION\#\#/$VERSION/g config_build.sh
+sed -i -e s/\#\#DEV\#\#/$DEV/g config_build.sh
+
+sed -i -e s/\#\#ID\#\#/$ID/g install.rdf install.js
+sed -i -e s/\#\#TAG\#\#/$TAG/g install.rdf install.js
+sed -i -e s/\#\#VERSION\#\#/$VERSION/g install.rdf install.js
+
 rm -rf dictionaries
 mkdir dictionaries
 cp -rf ../../resultats/hunspell/catalan-valencia.aff dictionaries/ca-ES-valencia.aff
 cp -rf ../../resultats/hunspell/catalan-valencia.dic dictionaries/ca-ES-valencia.dic
 ../../build-xpi.sh config_build.sh
 rm -rf dictionaries
+
+rm -rf install.rdf install.js config_build.sh
 
