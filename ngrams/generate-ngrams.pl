ae947bbadd35e82d58d035b0ee3988b5f28a3d02
jaumeortola <jaumeortola@gmail.com>
2015-02-15 11:15:22 +0100

    Genera 2-grams i 3-grams

--- ngrams/generate-ngrams.pl
+++ ngrams/generate-ngrams.pl
@@ -14,9 +14,10 @@ my $directory="/media/OS/Users/jaume/Documents/Llengua_catalana/Corpus/arreglat/
 #my $f1 = "tot.txt";
 #my $f2 = "adjectius.txt";
 #my $f1 = "toponims_sense_comuns.txt";
-my $out = "ngrams/unigrams.txt";
-my $out2 = "ngrams/errors.txt";
-my $out3 = "formes_trigrams.txt";
+my $outerrors = "ngrams/errors.txt";
+my $out1 = "ngrams/unigrams.txt";
+my $out2 = "ngrams/bigrams.txt";
+my $out3 = "ngrams/trigrams.txt";
 my %unigrams = ();
 my %bigrams = ();
 my %trigrams = ();
@@ -25,9 +26,7 @@ my $word2;
 my $line;
 my @matches;
 
-my $currentPos="";
-my $prevPos="";
-my $prevPrevPos="";
+my $wordcount=0;
 
 my @docs;
 my $file;
@@ -86,74 +85,86 @@ foreach $file (@docs) {
 
 	for my $sentence (@sentences) {
 	    #print "FRASE $sentence\n";
+	    my $prevWord="";
+	    my $prevPrevWord="";
 	    @matches = ($sentence =~ /$Flexio::carac+/g);
+#	    my $sentencepos=0;
 	    for $word (@matches)
 	    {
+		$wordcount++;
+#		$sentencepos++;
 		#print "PARAULA $word\n";
 		$word =~ s/^['\-]+//;
 		$word =~ s/['\-]+$//; 
 		$word2=$word;
-		if (!exists($diccionari{$word2}) && exists($diccionari{lc($word2)}))
-		{
+		if (!exists($diccionari{$word2}) && exists($diccionari{lc($word2)})) {
 		    $word2=lc($word2);
 		}
 		
-		if (exists($diccionari{$word2}))
-		{
-		    if (exists($unigrams{$word2}))
-		    {
+		if (exists($diccionari{$word2})) {
+		    if (exists($unigrams{$word2})) {
 			$unigrams{$word2}++;
-		    }
-		    else
-		    {
+		    } else {
 			$unigrams{$word2}=1;
 		    }
-		}
-		else 
-		{   
-		    if (exists($errors{$word}))
-		    {
-			$errors{$word}++;
+		    # bigram
+		    if ($prevWord =~ /.+/) {
+			if (exists($bigrams{$prevWord." ".$word2})) {
+			    $bigrams{$prevWord." ".$word2}++;
+			} else {
+			    $bigrams{$prevWord." ".$word2}=1;
+			}
+			# trigram
+			if ($prevPrevWord =~ /.+/) {
+			    if (exists($trigrams{$prevPrevWord." ".$prevWord." ".$word2})) {
+				$trigrams{$prevPrevWord." ".$prevWord." ".$word2}++;
+			    } else {
+				$trigrams{$prevPrevWord." ".$prevWord." ".$word2}=1;
+			    }
+			}
+			
 		    }
-		    else
-		    {
+		    $prevPrevWord=$prevWord;
+		    $prevWord=$word2;
+		} else {   
+		    if (exists($errors{$word})) {
+			$errors{$word}++;
+		    } else {
 			$errors{$word}=1;
 		    }
+		    $prevPrevWord="";
+		    $prevWord="";
 		}
 		
+
 =pod
-            if (exists($bigrams{$prevPos." ".$currentPos}))
+            if (exists($trigrams{$prevPrevWord." ".$prevWord." ".$currentPos}))
             {
-            $bigrams{$prevPos." ".$currentPos}++;
+            $trigrams{$prevPrevWord." ".$prevWord." ".$currentPos}++;
             }
             else
             {
-            $bigrams{$prevPos." ".$currentPos}=1;
-            }
-            if (exists($trigrams{$prevPrevPos." ".$prevPos." ".$currentPos}))
-            {
-            $trigrams{$prevPrevPos." ".$prevPos." ".$currentPos}++;
-            }
-            else
-            {
-            $trigrams{$prevPrevPos." ".$prevPos." ".$currentPos}=1;
+            $trigrams{$prevPrevWord." ".$prevWord." ".$currentPos}=1;
             }
 
-
-            $prevPrevPos=$prevPos;
-            $prevPos=$currentPos;
 =cut
-        }
+#            $prevPrevWord=$prevWord;
+#            $prevWord=$currentPos;
+
+	    }
 	}
 
     }
 
     close($fh);
 }
+
+print "Analitzades $wordcount paraules.\n";
+
 my $k;
 my $ofh;
 
-open($ofh, ">:encoding(UTF-8)", $out);
+open($ofh, ">:encoding(UTF-8)", $out1);
 foreach $k (sort {$unigrams{$b} <=> $unigrams{$a} } keys %unigrams)
 {
     print $ofh "$k\t$unigrams{$k}\n";
@@ -161,7 +172,7 @@ foreach $k (sort {$unigrams{$b} <=> $unigrams{$a} } keys %unigrams)
 close($ofh);
 
 
-open($ofh, ">:encoding(UTF-8)", $out2);
+open($ofh, ">:encoding(UTF-8)", $outerrors);
 foreach $k (sort {$errors{$b} <=> $errors{$a} } keys %errors)
 {
     print $ofh "$k\t$errors{$k}\n";
@@ -170,22 +181,21 @@ close($ofh);
 
 closedir (DIR);
 
-=pod
 open($ofh, ">:encoding(UTF-8)", $out2);
-foreach $k (sort {$bigrams{$a} <=> $bigrams{$b} }
-keys %bigrams)
+foreach $k (sort {$bigrams{$b} <=> $bigrams{$a} } keys %bigrams)
 {
-print $ofh "$k $bigrams{$k}\n";
+    print $ofh "$k\t$bigrams{$k}\n";
 }
 close($ofh);
+
 
 open($ofh, ">:encoding(UTF-8)", $out3);
-foreach $k (sort {$trigrams{$a} <=> $trigrams{$b} }
-keys %trigrams)
+foreach $k (sort {$trigrams{$b} <=> $trigrams{$a} } keys %trigrams)
 {
-print $ofh "$k $trigrams{$k}\n";
+    
+    print $ofh "$k\t$trigrams{$k}\n" if $trigrams{$k}>1;
 }
 close($ofh);
 
-=cut
+
 
