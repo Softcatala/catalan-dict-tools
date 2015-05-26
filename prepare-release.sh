9d7e54deadcd50602db12535b608c2cc23ae9316
jaumeortola <jaumeortola@gmail.com>
2015-05-26 09:57:08 +0200

    new script prepare-release

---
+++ prepare-release.sh
@@ -0,0 +1,38 @@
+VERSION="9.9.9"
+
+while getopts "v:" opt; do
+    case "$opt" in
+    v)  VERSION=$OPTARG;
+        ;;
+    esac
+done
+
+./build-lt.sh
+./build-hunspell.sh
+./build-oxt-all.sh -p -v '$VERSION'
+./build-xpi-all.sh -p -v '$VERSION' 
+
+mkdir resultats/release
+rm -rf resultats/release/*
+
+cp LICENSE resultats/release/
+cp release-notes_en.txt resultats/release/
+cp resultats/hunspell/* resultats/release/
+
+cp oxt/ca-valencia/ca-valencia.$VERSION.oxt resultats/release/
+cp oxt/ca/ca.$VERSION.oxt resultats/release/
+cp xpi/ca-valencia/ca-valencia.$VERSION.xpi resultats/release/
+cp xpi/ca/ca.$VERSION.xpi resultats/release/
+
+cd resultats/release
+zip ca.$VERSION-all.zip *
+zip ca.$VERSION-hunspell.zip catalan.aff catalan.dic LICENSE release-notes_en.txt
+zip ca-valencia.$VERSION-hunspell.zip catalan-valencia.aff catalan-valencia.dic LICENSE release-notes_en.txt
+
+rm LICENSE
+rm release-notes_en.txt
+rm *.dic
+rm *.aff
+
+cd ..
+echo "Paquets per a release en resultats/release"
