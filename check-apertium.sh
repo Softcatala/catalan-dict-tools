444de38396a563fe6eba2c5d941b04907a86cac2
Jaume Ortolà <jaumeortola@gmail.com>
2017-06-09 12:13:11 +0200

    check subst. apertium

--- check-apertium.sh
+++ check-apertium.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 dir_programes="fdic-to-apertium"
 original_apertium_dict="/home/jaume/apertium/apertium-cat/apertium-cat.cat.dix"
-generated_apertium_dict="resultats/apertium/adj-languagetool-format-apertium.txt"
+generated_apertium_adj_dict="resultats/apertium/adj-languagetool-format-apertium.txt"
+generated_apertium_nom_dict="resultats/apertium/nom-languagetool-format-apertium.txt"
 dir_resultat="tests-apertium"
 
 rm -rf $dir_resultat
 mkdir $dir_resultat
 
-perl $dir_programes/check-adj-apertium.pl $dir_programes/apertium-cat.pardefs $original_apertium_dict $generated_apertium_dict > $dir_resultat/check-apertium-adj.txt
+perl $dir_programes/check-adj-apertium.pl $dir_programes/apertium-cat.pardefs $original_apertium_dict $generated_apertium_adj_dict > $dir_resultat/check-apertium-adj.txt
+perl $dir_programes/check-nom-apertium.pl $dir_programes/apertium-cat.pardefs $original_apertium_dict $generated_apertium_nom_dict > $dir_resultat/check-apertium-nom.txt
 
 echo "Resultats en: $dir_resultat"
---
+++ fdic-to-apertium/check-nom-apertium.pl
@@ -0,0 +1,121 @@
+use strict;
+use warnings;
+use autodie;
+use utf8;
+
+binmode( STDOUT, ":utf8" );
+
+
+my $regles = $ARGV[0]; # paradigmes Apertium
+my $dict_entrada=$ARGV[1];
+my $dict_entrada2=$ARGV[2];
+
+open( my $fh,  "<:encoding(UTF-8)", $regles );
+
+my $inregla = 0;
+my @regles;
+my %nomsregles;
+my $spfx;
+my $regla ="";
+while (my $line = <$fh>) {
+    chomp($line);
+    if ($line =~ /<pardef n="(.*_n)".*>/) {
+		$regla = $1;
+		#$spfx =$2;
+		$inregla = 1;
+		my $sufix = "";
+		if ($regla =~ /\/(.*)__n/) {
+			$sufix = $1;
+		}
+		$nomsregles{$regla} = $sufix;
+    } elsif ($line =~ /^<\/pardef>/) {
+		$inregla = 0;
+    } elsif ($inregla) {
+		if ($line =~ /<e(.*?)>.*?<p><l>(.*?)<\/l>.*<r>(.*?)(<.*>)<\/r><\/p><\/e>/) {
+		    my $etiquetes=$4;
+		    my $lleva=$3;
+		    my $afig=$2;
+		    my $nombre = "S";
+		    if ($etiquetes =~ /"pl"/) { $nombre= "P";}
+		    if ($etiquetes =~ /"sp"/) { $nombre= "N";}
+		    my $genere = "M";
+		    if ($etiquetes =~ /"mf"/) { $genere= "C";}
+		    if ($etiquetes =~ /"f"/) { $genere= "F";}
+		    my $categoria = "NC";
+		    #if ($etiquetes =~ /"sup"/) { $categoria= "AQA";}
+		    my $postag= $categoria.$genere.$nombre."000";
+		    push (@regles, "$regla $postag $afig");
+			
+		}
+    }
+}
+close ($fh);
+
+@regles = sort @regles;
+
+my %regles_unalinia;
+
+my $linia = "";
+my $nomregla = "";
+my $prevnomregla = "-1";
+for my $linia_regla (@regles) { 	
+	if ($linia_regla =~ /(.*) (.*) (.*)/) {
+		$nomregla = $1; 
+		my $postag = $2;
+		my $afig = $3;
+		if ($nomregla !~ /^$prevnomregla$/) {
+			$regles_unalinia{$prevnomregla} = $linia;
+			# Comença nou adjectiu
+			$linia = "$postag <r>$afig";
+		} else {
+			$linia = $linia." $postag <r>$afig";
+		}
+		$prevnomregla = $nomregla;
+	}
+}
+$regles_unalinia{$prevnomregla} = $linia;
+
+
+
+my %dict1;
+open($fh,  "<:encoding(UTF-8)", $dict_entrada );
+while (my $line = <$fh>) {
+    chomp($line);
+    
+    if ($line =~ /<e lm="(.*)".*>.*<i>(.*)<\/i><par n="(.*__n)"\/><\/e>/) {
+    	my $lema=$1;
+    	my $nomregla=$3;
+    	my $arrel=$2;
+
+    	my $flexio_ap = $regles_unalinia{$nomregla};
+    	$flexio_ap =~ s/<r>/$arrel/g;
+    	$dict1{$lema} = $flexio_ap;
+    }
+}
+close ($fh);
+
+my %dict2;
+open($fh,  "<:encoding(UTF-8)", $dict_entrada2 );
+while (my $line = <$fh>) {
+    chomp($line);
+    
+    if ($line =~ /<e lm="(.*)".*>.*<i>(.*)<\/i><par n="(.*__n)"\/><\/e>/) {
+    	my $lema=$1;
+    	my $nomregla=$3;
+    	my $arrel=$2;
+    	if (exists $dict1{$lema}) {
+    		if (exists $regles_unalinia{$nomregla}) {
+	    		my $flexio_ap = $regles_unalinia{$nomregla};
+	    	    $flexio_ap =~ s/<r>/$arrel/g;
+	    	    if ( $dict1{$lema} !~ /^$flexio_ap$/) {
+	    	    	print "APERTIUM: $lema FORMES: $dict1{$lema}\n";
+	    	    	print "SOFTCATA: $lema FORMES: $flexio_ap\n\n";
+	    	    }
+    		} else {
+    			print "No existeix regla: $nomregla per al lema $lema.\n\n";
+    		}	
+    	}
+    }
+}
+close ($fh);
+
