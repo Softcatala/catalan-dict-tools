ebf693d7b131ca568b54c47e9c1a6a4bdc219f44
Jaume Ortolà <jaumeortola@gmail.com>
2017-06-21 16:58:37 +0200

    build apertium

--- build-apertium.sh
+++ build-apertium.sh
@@ -3,12 +3,13 @@ dir_programes="fdic-to-apertium"
 dir_dades="resultats/lt"
 dir_arrel="diccionari-arrel"
 dir_resultat="resultats/apertium"
+original_apertium_dict="/home/jaume/apertium/apertium-cat/apertium-cat.cat.dix"
 
 
 rm -rf $dir_resultat
 mkdir $dir_resultat
 
-perl $dir_programes/lt-to-apertium-adj.pl $dir_dades/diccionari.txt $dir_programes/apertium-cat.pardefs > $dir_resultat/adj-languagetool-format-apertium.txt
-perl $dir_programes/lt-to-apertium-nom.pl $dir_dades/diccionari.txt $dir_programes/apertium-cat.pardefs > $dir_resultat/nom-languagetool-format-apertium.txt
+perl $dir_programes/lt-to-apertium-adj.pl $dir_dades/diccionari.txt $original_apertium_dict > $dir_resultat/adj-languagetool-format-apertium.txt
+perl $dir_programes/lt-to-apertium-nom.pl $dir_dades/diccionari.txt $original_apertium_dict > $dir_resultat/nom-languagetool-format-apertium.txt
 
 echo "Resultats en: $dir_resultat"
