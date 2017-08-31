ed57c5a3290e08cc4b912411be6066cdd7d0b19f
Jaume Ortolà <jaumeortola@gmail.com>
2017-08-31 10:45:48 +0200

    comparació dnv

--- diccionari-arrel/dnv/compara-dnv.pl
+++ diccionari-arrel/dnv/compara-dnv.pl
@@ -7,13 +7,13 @@ binmode( STDOUT, ":utf8" );
 
 my $lt_dict="/home/jaume/github/catalan-dict-tools/resultats/lt/diccionari.txt";
 my $dnv_dict="/home/jaume/github/catalan-dict-tools/diccionari-arrel/dnv/LemesDNV.csv";
+my $mw_dict="/home/jaume/github/languagetool/languagetool-language-modules/ca/src/main/resources/org/languagetool/resource/ca/multiwords.txt"
 
 my %lt;
 my %dnv;
 open( my $lt_fh,  "<:encoding(UTF-8)", $lt_dict );
 open( my $dnv_fh,  "<:encoding(UTF-8)", $dnv_dict );
 
-#open( my $ofh, ">:encoding(UTF-8)", $out3 );
 
 while ( my $line = <$lt_fh> ) {
     chomp($line);
@@ -24,6 +24,21 @@ while ( my $line = <$lt_fh> ) {
     	}
     }
 }
+close $lt_fh;
+
+open( my $lt_fh,  "<:encoding(UTF-8)", $mw_dict );
+while ( my $line = <$lt_fh> ) {
+    chomp($line);
+    if ($line =~ /^(.+)\t(.+)$/) {
+    	my $lemma = $1;
+    	if (!exists $lt{$lemma}) {
+    		$lt{$lemma}=1;
+    	}
+    }
+}
+close $lt_fh;
+
+
 #my $size = scalar keys %lt;
 #print "El diccionari LT té ". $size . " formes\n";
 
