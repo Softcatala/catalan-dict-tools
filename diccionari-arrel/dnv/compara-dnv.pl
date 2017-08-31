8d02bcd5900040014c5c194b0b487faed0694757
Jaume Ortolà <jaumeortola@gmail.com>
2017-08-31 12:20:27 +0200

    cont. class. dnv

--- diccionari-arrel/dnv/compara-dnv.pl
+++ diccionari-arrel/dnv/compara-dnv.pl
@@ -8,6 +8,7 @@ binmode( STDOUT, ":utf8" );
 my $lt_dict="/home/jaume/github/catalan-dict-tools/resultats/lt/diccionari.txt";
 my $dnv_dict="/home/jaume/github/catalan-dict-tools/diccionari-arrel/dnv/LemesDNV.csv";
 my $mw_dict="/home/jaume/github/languagetool/languagetool-language-modules/ca/src/main/resources/org/languagetool/resource/ca/multiwords.txt";
+my $mc_dict="/home/jaume/github/catalan-dict-tools/diccionari-arrel/dnv/mots-classificats.txt";
 
 my %lt;
 my %dnv;
@@ -38,6 +39,17 @@ while ( my $line = <$lt_fh> ) {
 }
 close $lt_fh;
 
+open( $lt_fh,  "<:encoding(UTF-8)", $mc_dict );
+while ( my $line = <$lt_fh> ) {
+    chomp($line);
+    if ($line =~ /^([^# ]+)/) {
+    	my $lemma = $1;
+    	if (!exists $lt{$lemma}) {
+    		$lt{$lemma}=1;
+    	}
+    }
+}
+close $lt_fh;
 
 #my $size = scalar keys %lt;
 #print "El diccionari LT té ". $size . " formes\n";
