bbb23c218ab995564f21006ecdcb597ebfc3d20a
Jaume Ortolà <jaumeortola@gmail.com>
2017-12-16 00:12:38 +0100

    més paraules dnv

--- diccionari-arrel/dnv/compara-dnv.pl
+++ diccionari-arrel/dnv/compara-dnv.pl
@@ -5,17 +5,16 @@ use autodie;
 use utf8;
 binmode( STDOUT, ":utf8" );
 
-my $lt_dict="/home/jaume/github/catalan-dict-tools/resultats/lt/diccionari-dnv.txt";
+my $lt_dict="/home/jaume/github/catalan-dict-tools/resultats/lt/diccionari.txt";
+my $lt_dict2="/home/jaume/github/catalan-dict-tools/resultats/lt/diccionari-dnv.txt";
 my $dnv_dict="/home/jaume/github/catalan-dict-tools/diccionari-arrel/dnv/LemesDNV.csv";
 my $mw_dict="/home/jaume/github/languagetool/languagetool-language-modules/ca/src/main/resources/org/languagetool/resource/ca/multiwords.txt";
 my $mc_dict="/home/jaume/github/catalan-dict-tools/diccionari-arrel/dnv/mots-classificats.txt";
 
 my %lt;
 my %dnv;
+
 open( my $lt_fh,  "<:encoding(UTF-8)", $lt_dict );
-open( my $dnv_fh,  "<:encoding(UTF-8)", $dnv_dict );
-
-
 while ( my $line = <$lt_fh> ) {
     chomp($line);
     if ($line =~ /(.+) (.+) (.+)/) {
@@ -23,6 +22,18 @@ while ( my $line = <$lt_fh> ) {
     	if (!exists $lt{$lemma}) {
     		$lt{$lemma}=1;
     	}
+    }
+}
+close $lt_fh;
+
+open( $lt_fh,  "<:encoding(UTF-8)", $lt_dict2 );
+while ( my $line = <$lt_fh> ) {
+    chomp($line);
+    if ($line =~ /(.+) (.+) (.+)/) {
+        my $lemma = $1;
+        if (!exists $lt{$lemma}) {
+            $lt{$lemma}=1;
+        }
     }
 }
 close $lt_fh;
@@ -59,6 +70,7 @@ close $lt_fh;
 #my $size = scalar keys %lt;
 #print "El diccionari LT té ". $size . " formes\n";
 
+open( my $dnv_fh,  "<:encoding(UTF-8)", $dnv_dict );
 while ( my $line = <$dnv_fh> ) {
     chomp($line);
     if ($line =~ /(.*);(.*);(.*);(.*);(.*);(.*);/) {
@@ -72,6 +84,8 @@ while ( my $line = <$dnv_fh> ) {
     	}
     }
 }
+close($dnv_fh);
+
 #$size = scalar keys %dnv;
 #print "El diccionari DNV té ". $size . " lemes\n";
 
