eef1ad422912c90135a250321c66cf28a0751a98
Jaume Ortolà <jaumeortola@gmail.com>
2018-10-08 23:20:38 +0200

    re-indent

--- fdic-to-hunspell/fdic-to-hunspell-verbs.pl
+++ fdic-to-hunspell/fdic-to-hunspell-verbs.pl
@@ -9,20 +9,21 @@ require "./libs/Flexio.pm";
 
 binmode( STDOUT, ":utf8" );
 
-my $f1   = $ARGV[0]; #"diccionari-arrel/verbs-fdic.txt";
-my $out  = $ARGV[1]; #"verbs-hunspell.dic";
-my $modelsdir = $ARGV[2]."/"; #"diccionari-arrel/models-verbals/";
+my $f1        = $ARGV[0];          #"diccionari-arrel/verbs-fdic.txt";
+my $out       = $ARGV[1];          #"verbs-hunspell.dic";
+my $modelsdir = $ARGV[2] . "/";    #"diccionari-arrel/models-verbals/";
 
-my @files = glob($modelsdir."*.model");
+my @files = glob( $modelsdir . "*.model" );
 @files = sort(@files);
 my $modelscount = 0;
-my %sufixos = ();
+my %sufixos     = ();
+
 #llegeix nom dels models i assigna sufix hexadeximal 01..9B
 foreach my $file (@files) {
-    next if ($file !~ /\.model$/);
+    next if ( $file !~ /\.model$/ );
     $modelscount++;
-    my $sufix= sprintf ("%02X", $modelscount);
-    my $infinitiu=decode("utf8",$file);
+    my $sufix = sprintf( "%02X", $modelscount );
+    my $infinitiu = decode( "utf8", $file );
     $infinitiu =~ s/$modelsdir(.*)\.model/$1/;
     $sufixos{$infinitiu} = $sufix;
 }
@@ -30,52 +31,58 @@ foreach my $file (@files) {
 open( my $fh,  "<:encoding(UTF-8)", $f1 );
 open( my $ofh, ">:encoding(UTF-8)", $out );
 my %formes = ();
-while (my $line = <$fh>) {
+while ( my $line = <$fh> ) {
     chomp($line);
-    if ($line =~ /^([^#]+)(er|re|ir|ar|r)=categories:(.+?);model:(.+?);/) { 
-	my $infinitiu = $1.$2;
-	my $terminacio = $2;
-	my $categoria = $3;
-	my $model = $4;
+    if ( $line =~ /^([^#]+)(er|re|ir|ar|r)=categories:(.+?);model:(.+?);/ ) {
+        my $infinitiu  = $1 . $2;
+        my $terminacio = $2;
+        my $categoria  = $3;
+        my $model      = $4;
 
-	#infinitiu amb el seu model
-	#my $apostrofainfinitiu = "";
-	#if ( Flexio::apostrofa_masculi($infinitiu) ) {
-	#    $apostrofainfinitiu = "_V";
-	#}
-	if ($model =~ /^cantar$/ && !Flexio::apostrofa_masculi($infinitiu)) {
-	    print $ofh "$infinitiu/00\n"; # model especial sense apostrofació: cantar, iodar, halar, etc.
-	    # NO SERVEIX PER A HACKEJAR, perquè no és del model "cantar" sinó "envejar"
-	} else {
-	    print $ofh "$infinitiu/$sufixos{$model}\n"; #$apostrofainfinitiu\n";
-	}
-	
+        #infinitiu amb el seu model
+        #my $apostrofainfinitiu = "";
+        #if ( Flexio::apostrofa_masculi($infinitiu) ) {
+        #    $apostrofainfinitiu = "_V";
+        #}
+        if ( $model =~ /^cantar$/ && !Flexio::apostrofa_masculi($infinitiu) ) {
+            print $ofh "$infinitiu/00\n"
+              ; # model especial sense apostrofació: cantar, iodar, halar, etc.
+             # NO SERVEIX PER A HACKEJAR, perquè no és del model "cantar" sinó "envejar"
+        }
+        else {
+            print $ofh "$infinitiu/$sufixos{$model}\n"; #$apostrofainfinitiu\n";
+        }
+
         # participi femení singular amb l'
-	if ($infinitiu =~ /^h?[aeo]/) {
-	    open( my $modelfh,  "<:encoding(UTF-8)", $modelsdir.$model.".model" );
-	    while (my $modelline = <$modelfh>) {
-		if ($modelline =~ /^(.+) (.+) (.+) (.+) #.*$/) {
-		    my $forma = $infinitiu;
-		    my $trau = $1;
-		    my $afegeix = $2;
-		    my $postag = $4;
-		    if ($forma =~ /^(.*)$trau$/) {
-			$forma = $1;
-		    }
-		    else {
-			print $ofh "!!!!ERROR en $forma\n";
-		    }
-		    if ($afegeix !~ /^0$/) {
-			$forma .= $afegeix;
-		    }
-		    if ($postag =~ /^V.P..SF.$/ && Flexio::apostrofa_femeni($forma) && $forma !~/espesa/) {
-			print $ofh "$forma/_V_Y\n"
-		    }
-		}
-	    }
-	    close ($modelfh);
-	}
+        if ( $infinitiu =~ /^h?[aeo]/ ) {
+            open( my $modelfh, "<:encoding(UTF-8)",
+                $modelsdir . $model . ".model" );
+            while ( my $modelline = <$modelfh> ) {
+                if ( $modelline =~ /^(.+) (.+) (.+) (.+) #.*$/ ) {
+                    my $forma   = $infinitiu;
+                    my $trau    = $1;
+                    my $afegeix = $2;
+                    my $postag  = $4;
+                    if ( $forma =~ /^(.*)$trau$/ ) {
+                        $forma = $1;
+                    }
+                    else {
+                        print $ofh "!!!!ERROR en $forma\n";
+                    }
+                    if ( $afegeix !~ /^0$/ ) {
+                        $forma .= $afegeix;
+                    }
+                    if (   $postag =~ /^V.P..SF.$/
+                        && Flexio::apostrofa_femeni($forma)
+                        && $forma !~ /espesa/ )
+                    {
+                        print $ofh "$forma/_V_Y\n";
+                    }
+                }
+            }
+            close($modelfh);
+        }
     }
 }
-close ($ofh);
-close ($fh);
+close($ofh);
+close($fh);
--- fdic-to-hunspell/genera-modelsverbals-hunspell.pl
+++ fdic-to-hunspell/genera-modelsverbals-hunspell.pl
@@ -8,91 +8,103 @@ no locale;
 
 binmode( STDOUT, ":utf8" );
 
-my $general=0; #Si és 1, versió "general" del corrector
+my $general = 0;    #Si és 1, versió "general" del corrector
 if ( grep( /^-catalan$/, @ARGV ) ) {
-  $general=1;
+    $general = 1;
 }
 
-
-my $modelsdir = $ARGV[0]."/";
-my @files = glob($modelsdir."*.model");
+my $modelsdir = $ARGV[0] . "/";
+my @files     = glob( $modelsdir . "*.model" );
 my @regles;
 @files = sort (@files);
 my $modelscount = 0;
-my $afffile = $ARGV[1];
+my $afffile     = $ARGV[1];
 open( my $ofh, ">:encoding(UTF-8)", $afffile );
 foreach my $file (@files) {
-    next if ($file !~ /\.model$/);
+    next if ( $file !~ /\.model$/ );
     $modelscount++;
-    my $sufix= sprintf ("%02X", $modelscount);
-    my $infinitiu=decode("utf8",$file);
+    my $sufix = sprintf( "%02X", $modelscount );
+    my $infinitiu = decode( "utf8", $file );
     $infinitiu =~ s/$modelsdir(.*)\.model/$1/;
-    open( my $modelfh,  "<:encoding(UTF-8)", $file );
-    my $compta=0;
-    LINE: while (my $modelline = <$modelfh>) {
-	if ($modelline =~ /^(.+) (.+) (.+) (.+) #.*$/ && $modelline !~ /IGNOREHUNSPELL/) {
-	    my $trau = $1;
-	    my $afegeix = $2;
-	    my $condiciofinal = $3;
-	    my $postag = $4;
-	    my $afixos="";
-	    my $forma=$infinitiu;
-	    if ($forma =~ /^(.*)$trau$/) {
-		$forma = $1;
-	    }
-	    else {
-		print $ofh "!!!!ERROR en $forma\n";
-	    }
-	    if ($afegeix !~ /^0$/) {
-		$forma .= $afegeix;
-	    }
+    open( my $modelfh, "<:encoding(UTF-8)", $file );
+    my $compta = 0;
+  LINE: while ( my $modelline = <$modelfh> ) {
 
-	    #Elimina accentuació valenciana del diccionari general
-	    if ($general) {
-		next LINE if ($postag =~ /^V.P.*$/ && $forma =~ /és$/);
-		next LINE if ($postag =~ /^V.N.*/ && $forma =~ /é(ixer|nyer|ncer)$/ && $forma !~ /^(cr|acr|decr|n|p|recr|ren|sobrecr|sobren)éixer$/);
-		next LINE if ($forma =~ /éie[mu]$/);
-		next LINE if ($forma =~ /^(apreh|apr|carv|compr|corpr|def|dep|desapr|desp|despr|depr|empr|exp|malpr|malv|mampr|of|repr|retrov|rev|salpr|sobrepr|sorpr|susp|ullpr)én$/);
+        if (   $modelline =~ /^(.+) (.+) (.+) (.+) #.*$/
+            && $modelline !~ /IGNOREHUNSPELL/ )
+        {
+            my $trau          = $1;
+            my $afegeix       = $2;
+            my $condiciofinal = $3;
+            my $postag        = $4;
+            my $afixos        = "";
+            my $forma         = $infinitiu;
+            if ( $forma =~ /^(.*)$trau$/ ) {
+                $forma = $1;
+            }
+            else {
+                print $ofh "!!!!ERROR en $forma\n";
+            }
+            if ( $afegeix !~ /^0$/ ) {
+                $forma .= $afegeix;
+            }
+
+            #Elimina accentuació valenciana del diccionari general
+            if ($general) {
+                next LINE if ( $postag =~ /^V.P.*$/ && $forma =~ /és$/ );
+                next LINE if ( $postag =~ /^V.N.*/
+                    && $forma =~ /é(ixer|nyer|ncer)$/
+                    && $forma !~ /^(cr|acr|decr|n|p|recr|ren|sobrecr|sobren)éixer$/ );
+                next LINE if ( $forma =~ /éie[mu]$/ );
+                next LINE if ( $forma =~ /^(apreh|apr|carv|compr|corpr|def|dep|desapr|desp|despr|depr|empr|exp|malpr|malv|mampr|of|repr|retrov|rev|salpr|sobrepr|sorpr|susp|ullpr)én$/  );
                 #<!-- Cas molt rar que no es té en compte: "revén" és vàlid en accentuació general com a imperatiu de revenir. No és vàlid si ve de revendre.  -->
-	    }
+            }
 
-	    if ($postag =~ /^V.N.*$/) {
-		if ($forma =~ /[^e]$/) {
-		    $afixos="_v_C_Y"; #infinitiu acabat en consonant
-		} else {
-		    $afixos="_v_D_Y"; #infinitiu acabat en vocal
-		}
-	    } elsif ($postag =~ /^V.G.*$/) {
-		$afixos="_C"; #gerundi
-	    } elsif ($postag =~ /^V.P..SM.$/) {
-		$afixos="_v_Y"; #participi MS
-	    } elsif ($postag =~ /^V.P..SF.$/) {
-		$afixos="_Y"; #participi FS **** Falta afegir l'apostrofació l' (_V) en la forma evitant les excepcions. 
-	    } elsif ($postag =~ /^V.P..P..$/) {
-		$afixos="_Y"; #participi P
-	    } elsif ($postag =~ /^V.M.*$/) {
-		if ($forma =~ /[aeiï]$/) {
-		    $afixos="_D"; #imperatiu acabat en vocal: a, e, i, ï 
-		} else {
-		    $afixos="_C"; #imperatiu acabat en consonat o u
-		}		
-	    }
-	    else {
-		$afixos="_Z";
-	    }
-	    $compta++;	
-	    push (@regles, "SFX $sufix $trau $afegeix/$afixos $condiciofinal");
-	}
+            if ( $postag =~ /^V.N.*$/ ) {
+                if ( $forma =~ /[^e]$/ ) {
+                    $afixos = "_v_C_Y";    #infinitiu acabat en consonant
+                }
+                else {
+                    $afixos = "_v_D_Y";    #infinitiu acabat en vocal
+                }
+            }
+            elsif ( $postag =~ /^V.G.*$/ ) {
+                $afixos = "_C";            #gerundi
+            }
+            elsif ( $postag =~ /^V.P..SM.$/ ) {
+                $afixos = "_v_Y";          #participi MS
+            }
+            elsif ( $postag =~ /^V.P..SF.$/ ) {
+                $afixos = "_Y"
+                  ; #participi FS **** Falta afegir l'apostrofació l' (_V) en la forma evitant les excepcions.
+            }
+            elsif ( $postag =~ /^V.P..P..$/ ) {
+                $afixos = "_Y";    #participi P
+            }
+            elsif ( $postag =~ /^V.M.*$/ ) {
+                if ( $forma =~ /[aeiï]$/ ) {
+                    $afixos = "_D";    #imperatiu acabat en vocal: a, e, i, ï
+                }
+                else {
+                    $afixos = "_C";    #imperatiu acabat en consonat o u
+                }
+            }
+            else {
+                $afixos = "_Z";
+            }
+            $compta++;
+            push( @regles, "SFX $sufix $trau $afegeix/$afixos $condiciofinal" );
+        }
     }
 
     print $ofh "\n# Model de conjugació: $infinitiu\n";
     print $ofh "SFX $sufix Y $compta\n";
     for my $liniaregla (@regles) {
-	print $ofh "$liniaregla\n";
+        print $ofh "$liniaregla\n";
     }
     undef @regles;
-    close ($modelfh);
+    close($modelfh);
 
 }
 
-close ($ofh);
+close($ofh);
