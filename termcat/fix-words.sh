1aa7561bb2de4136ecd5b488af4c8b8a73f16526
Jaume Ortolà <jaumeortola@gmail.com>
2019-07-21 16:20:32 +0200

    fixwords

---
+++ termcat/fix-words.sh
@@ -0,0 +1,9 @@
+cd xml-dicts
+sed -i 's/biospeleologia/bioespeleologia/' *.xml
+sed -i 's/vitelí/vitel·lí/' *.xml
+sed -i 's/excresència/excrescència/' *.xml
+sed -i 's/antiarítmic/antiarrítmic/' *.xml
+sed -i 's/corepressor/correpressor/' *.xml
+sed -i 's/electrostimulador/electroestimulador/' *.xml
+sed -i 's/pupil·lostatòmetre/pupil·loestatòmetre/' *.xml
+cd ..
