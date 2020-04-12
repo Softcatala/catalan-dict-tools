40050683e9f6a580a29f9db8066464b00852f81a
jaumeortola <jaumeortola@gmail.com>
2020-04-12 10:15:07 +0200

    fix wordlist

--- build-wordlist-from-lt.sh
+++ build-wordlist-from-lt.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-perl ./wordlist/wordlist.pl ./resultats/lt/diccionari.txt ./resultats/hunspell/catalan-valencia.aff ./resultats/wordlist/wordlist.txt
-cat fdic-to-hunspell/dades/extres.dic >> ./resultats/wordlist/wordlist.txt
-export LC_ALL=C && sort -u ./resultats/wordlist/wordlist.txt -o ./resultats/wordlist/wordlist.txt
+perl ./wordlist/wordlist.pl ./resultats/lt/diccionari.txt ./resultats/hunspell/catalan-valencia.aff ./resultats/spelling/wordlist.txt
+cat fdic-to-hunspell/dades/extres.dic >> ./resultats/spelling/wordlist.txt
+export LC_ALL=C && sort -u ./resultats/spelling/wordlist.txt -o ./resultats/spelling/wordlist.txt
+
+sed -i -E 's/\s+$//' ./resultats/spelling/wordlist.txt
+sed -i '/^$/d' ./resultats/spelling/wordlist.txt
