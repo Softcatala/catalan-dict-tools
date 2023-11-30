8e36cba5b21aaffafcdb1ed9b5e17cc30ec43d76
Jaume Ortolà <jaumeortola@gmail.com>
2023-11-30 14:20:33 +0100

    ecoansietat

--- update-morfologik-version.sh
+++ update-morfologik-version.sh
@@ -1,4 +1,4 @@
-dictVers=2.22
+dictVers=2.23
 unzip ~/.m2/repository/org/softcatala/catalan-pos-dict/${dictVers}/catalan-pos-dict-${dictVers}.jar
 cp org/languagetool/resource/ca/* morfologik-lt/
 rm -rf org/
