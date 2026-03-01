76cb2ec123ab985c0930421bbc139e37fed66c32
jaumeortola <jaumeortola@gmail.com>
2026-03-01 22:00:40 +0100

    update morfologik version

--- update-morfologik-version.sh
+++ update-morfologik-version.sh
@@ -1,4 +1,4 @@
-dictVers=2.27
+dictVers=3.0
 unzip ~/.m2/repository/org/softcatala/catalan-pos-dict/${dictVers}/catalan-pos-dict-${dictVers}.jar
 cp org/languagetool/resource/ca/* morfologik-lt/
 rm -rf org/
